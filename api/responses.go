a6a43a5840ecda4167e441214f576dd8ba5e21af
k0marov <95040709+k0marov@users.noreply.github.com>
2022-07-22 19:24:45 +0300

    add limits field to UserInfoResponse

--- api/responses.go
+++ api/responses.go
@@ -1,8 +1,14 @@
 package api
 
+type LimitResponse struct {
+	Withdrawn float64 `json:"withdrawn"` 
+	Max       float64 `json:"max"` 
+}
+
 type UserInfoResponse struct {
 	Id     string             `json:"id"`
-	Wallet map[string]float64 `json:"wallet"`
+	Wallet map[string]float64 `json:"wallet"`  // key is currency, value is money amount
+	Limits map[string]LimitResponse `json:"limits"`  // key is currency, value is limit
 }
 type VerifiedCodeResponse struct {
 	UserInfo UserInfoResponse `json:"user_info"`
