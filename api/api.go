e75ff997b82d9afd425454a7a7373cc7f22724a8
k0marov <95040709+k0marov@users.noreply.github.com>
2022-07-23 12:58:45 +0300

    add a user-info endpoint

--- api/api.go
+++ api/api.go
@@ -10,32 +10,36 @@ const TransactionTypeQueryArg = "transaction_type"
 const TransactionTypeDeposit = "deposit"
 const TransactionTypeWithdrawal = "withdrawal"
 
-func NewAPIRouter(atmTransaction ATMTransaction, authMiddleware Middleware) http.Handler {
+func NewAPIRouter(h Handlers, authMiddleware Middleware) http.Handler {
 	r := chi.NewRouter()
 
 	r.Route("/api/v1", func(r chi.Router) {
 		r.Route("/atm-transaction", func(r chi.Router) {
 			// requires a TransactionTypeQueryArg
 			// Response: CodeResponse
-			// Throws: TransactionTypeNotProvided
-			r.Get("/gen-code", authMiddleware(atmTransaction.GenCode).ServeHTTP)
+			// Throws: 401, TransactionTypeNotProvided
+			r.Get("/gen-code", authMiddleware(h.GenCode).ServeHTTP)
 
 			// Request: CodeRequest; requires a TransactionTypeQueryArg
 			// Response: VerifiedCodeResponse
 			// Throws: TransactionTypeNotProvided, InvalidCode, InvalidTransactionType
-			r.Post("/verify-code", atmTransaction.VerifyCode)
+			r.Post("/verify-code", h.VerifyCode)
 
 			// Request: BanknoteCheckRequest
 			// Response: 200 if accepted, client error (or 500) if rejected
 			// Throws: InvalidCode (means session-expired)
-			r.Post("/check-banknote", atmTransaction.CheckBanknote)
+			r.Post("/check-banknote", h.CheckBanknote)
 
 			// Request: FinalizeTransactionRequest
 			// Response: 200 if accepted, client error (or 500) if rejected
 			// Throws: InvalidATMSecret, InsufficientFunds
-			r.Post("/finalize-transaction", atmTransaction.FinalizeTransaction)
+			r.Post("/finalize-transaction", h.FinalizeTransaction)
 		})
+		// Response: UserInfoResponse 
+		// Throws: 401
+		r.Get("user-info", authMiddleware(h.GetUserInfo).ServeHTTP)
 	})
+	
 
 	return r
 }
