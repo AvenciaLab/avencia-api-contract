1d5db47ddcaa4ed858469de6fe7573f77b092dca
Sam Komarov <sam@skomarov.com>
2022-08-01 09:12:29 +0300

    use a money struct instead of separate Currency and Amount fields

--- api/requests.go
+++ api/requests.go
@@ -13,14 +13,11 @@ type FinalizeTransactionRequest struct {
 	UserId    string            `json:"user_id"`
 	ATMSecret string            `json:"atm_secret"`
 	Source    TransactionSource `json:"source"`
-	// TODO: change to use Money struct
-	Currency  string            `json:"currency"`
-	Amount    float64           `json:"amount"` // negative value means withdrawal
+	Money     Money             `json:"money"`
 }
 
 // Requested from the Avencia App
 type TransferRequest struct {
 	RecipientIdentifier string  `json:"recipient_identifier"` // currently it's email, maybe later it will be a phone number
-	Currency            string  `json:"currency"`
-	Amount              float64 `json:"amount"` // can only be positive for obvious reasons
+	Money Money `json:"money"`
 }
