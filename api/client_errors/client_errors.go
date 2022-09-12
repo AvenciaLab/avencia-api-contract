773af9a65bf43e16154f544773a00e611538c1fe
Sam Komarov <sam@skomarov.com>
2022-09-12 07:05:50 +0300

    replace the error codes with display messages

--- api/client_errors/client_errors.go
+++ api/client_errors/client_errors.go
@@ -3,65 +3,65 @@ package client_errors
 import "fmt"
 
 type ClientError struct {
-	DetailCode string `json:"detail_code"`
+	DisplayMessage string `json:"displayMessage"`
 	HTTPCode   int    `json:"-"`
 }
 
 func (ce ClientError) Error() string {
-	return fmt.Sprintf("An error which will be displayed to the client: %v %v", ce.HTTPCode, ce.DetailCode)
+	return fmt.Sprintf("An error which will be displayed to the client: %v %v", ce.HTTPCode, ce.DisplayMessage)
 }
 
 var InvalidCode = ClientError{
-	DetailCode: "invalid-code",
+	DisplayMessage: "The provided QR Code is invalid.",
 	HTTPCode:   400,
 }
 
 var InvalidTransactionType = ClientError{
-	DetailCode: "invalid-transaction-type",
+	DisplayMessage: "Invalid transaction type.",
 	HTTPCode:   400,
 }
 
 var TransactionIdNotProvided = ClientError{
-	DetailCode: "transaction-id-not-provided",
+	DisplayMessage: "Transaction ID was not provided.",
 	HTTPCode:   400,
 }
 
 var InvalidATMSecret = ClientError{
-	DetailCode: "invalid-atm-secret",
+	DisplayMessage: "The provided ATM Authentication secret is invalid.",
 	HTTPCode:   401,
 }
 
 var InsufficientFunds = ClientError{
-	DetailCode: "insufficient-funds",
+	DisplayMessage: "Not enough funds to perform the operation.",
 	HTTPCode:   400,
 }
 
 var WithdrawLimitExceeded = ClientError{
-	DetailCode: "withdraw-limit-exceeded",
+	DisplayMessage: "Withdraw limit exceeded.",
 	HTTPCode: 400,
 }
 
 var NotFound = ClientError{
-	DetailCode: "not-found", 
+	DisplayMessage: "Not Found.", 
 	HTTPCode: 404, 
 }
 
 var NegativeTransferAmount = ClientError{
-	DetailCode: "negative-transfer-amount", 
+	DisplayMessage: "Negative transfer amount.", 
 	HTTPCode: 400, 
 }
 
 var InvalidJSON = ClientError{
-	DetailCode: "invalid-json", 
+	DisplayMessage: "Invalid request format.", 
 	HTTPCode: 400, 
 }
 
 var TransferringToYourself = ClientError{
-	DetailCode: "transferring-yourself", 
+	DisplayMessage: "You cannot transfer to yourself.", 
 	HTTPCode: 400, 
 }
 
 var TransferringZero = ClientError{
-	DetailCode: "transferring-zero", 
+	DisplayMessage: "Transferring zero.", 
 	HTTPCode: 400, 
 }
